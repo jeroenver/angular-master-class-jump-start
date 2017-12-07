21192d6eb9493111c5e5249ca46b3d3ed9e56ab1
Jeroen Verellen <jeroen.verellen@gmail.com>
2017-12-07 15:33:55 +0100

    exercise reactive form

--- src/app/contacts-creator/contacts-creator.component.ts
+++ src/app/contacts-creator/contacts-creator.component.ts
@@ -2,6 +2,11 @@ import {Component, OnInit} from '@angular/core';
 import {Contact} from '../models/contact';
 import {ContactsService} from '../contacts.service';
 import {Router} from '@angular/router';
+import {checkEmailAvailability} from '../email-availability-validator.directive';
+import {validateEmail} from '../email-validator.directive';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {GENDER} from '../data/gender';
+import {COUNTRIES_DATA} from '../data/countries-data';
 
 @Component({
   selector: 'trm-contacts-creator',
@@ -9,11 +14,28 @@ import {Router} from '@angular/router';
   styleUrls: ['./contacts-creator.component.css']
 })
 export class ContactsCreatorComponent implements OnInit {
+  contactForm: FormGroup;
+  gender = GENDER;
+  countries = COUNTRIES_DATA;
 
-  constructor(private contactsService: ContactsService, private router: Router) {
+  constructor(private contactsService: ContactsService, private router: Router, private fb: FormBuilder) {
   }
 
   ngOnInit() {
+    this.contactForm = this.fb.group({
+      'name': ['', [Validators.required, Validators.minLength(3)]],
+      'email': ['', validateEmail, checkEmailAvailability(this.contactsService)],
+      'phone': '',
+      'gender': '',
+      'birthday': '',
+      'website': '',
+      'address': this.fb.group({
+        'street': '',
+        'zip': '',
+        'city': '',
+        'country': ''
+      })
+    });
   }
 
   save(contact: Contact) {
